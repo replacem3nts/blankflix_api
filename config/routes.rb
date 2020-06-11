57cb428f971badb3a6328e96fad0b187b7abcf98
replacem3nts <donny.landis@gmail.com>
2020-06-10 21:09:21 -0400

    small tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/users/persist_login', to: 'users#persist_login'
       post '/users/login', to: 'users#login'
-      get '/users/:id', to: 'users#show'
       post '/users', to: 'users#create'
       patch '/users', to: 'users#update'
       delete '/users', to: 'users#destroy'
       
-      get '/movies', to: 'movies#index'
-      get '/movies/:id', to: 'movies#show'
       post '/movies', to: 'movies#create'
+      patch '/movies', to: 'movies#update'
       delete '/movies/:id', to: 'movies#delete'
     end
   end
